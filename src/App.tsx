7944ab3192450128eafa5b87e6eda15e7054aa2b
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-04-23 01:20:08 +0200

    SkillWallet implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div className="text-xl text-red-500">Test</div>;
-}
+import { InitSwAuth } from "@skill-wallet/auth";
+import { useEffect } from "react";
+
+const App = () => {
+  useEffect(() => {
+    InitSwAuth();
+  }, []);
+  return (
+    <div className="text-xl text-red-500">
+      {/* @ts-ignore */}
+      <sw-auth
+        partner-key="d2af0de3d704460251bb855baa51f84280dbbc65"
+        use-dev="true"
+        use-button-options="true"
+      />
+    </div>
+  );
+};
 
 export default App;
